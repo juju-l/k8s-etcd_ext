bde459d8a196cd76b57fec9d75417159356bed14
root <lj@vipex.cc>
2021-10-20 13:37:54 +0800

    修复一些启动问题

--- dep_kubernetes-etcd.sh
+++ dep_kubernetes-etcd.sh
@@ -11,21 +11,21 @@ chmod -R 700 ~/.k8s-etcds
 cat <<EOF > ~/.k8s-etcds/.start.sh
 
 
-hostIp=\$(ip addr|\\
-  grep global|grep brd|grep -v docker|head -1|awk '{print $2}'|\\
-cut -d '/' -f 1)
-etcd --advertise-client-urls=https://${hostIp}:2379 \\
+#hostIp=\$(ip addr|\\
+#  grep global|grep brd|grep -v docker|head -1|awk '{print $2}'|\\
+#cut -d '/' -f 1)
+etcd --advertise-client-urls=https://\${hostIp}:2379 \\
   --cert-file=/etc/kubernetes/pki/etcd/server.crt --client-cert-auth=true \\
   --data-dir=/var/lib/etcd \\
-  --initial-advertise-peer-urls=https://${hostIp}:2380 --initial-cluster="mater-${hostIp}-2379=https://${hostIp}:2380,slave-10.0.1.102-2379=https://10.0.1.102:2380,slave-10.0.1.103-2379=https://10.0.1.103:2380" \\
+  --initial-advertise-peer-urls=https://\${hostIp}:2380 --initial-cluster="mater-\${hostIp}-2379=https://\${hostIp}:2380,slave-10.0.1.102-2379=https://10.0.1.102:2380,slave-10.0.1.103-2379=https://10.0.1.103:2380" \\
   --key-file=/etc/kubernetes/pki/etcd/server.key \\
-  --listen-client-urls="https://127.0.0.1:2379,https://${hostIp}:2379" --listen-metrics-urls=http://127.0.0.1:2381 --listen-peer-urls=https://${hostIp}:2380 \\
-  --name=mater-${hostIp}-2379 \\
+  --listen-client-urls="https://127.0.0.1:2379,https://\${hostIp}:2379" --listen-metrics-urls=http://127.0.0.1:2381 --listen-peer-urls=https://\${hostIp}:2380 \\
+  --name=mater-\${hostIp}-2379 \\
   --peer-cert-file=/etc/kubernetes/pki/etcd/peer.crt --peer-client-cert-auth=true --peer-key-file=/etc/kubernetes/pki/etcd/peer.key --peer-trusted-ca-file=/etc/kubernetes/pki/etcd/ca.crt \\
   --snapshot-count=10000 \\
   --trusted-ca-file=/etc/kubernetes/pki/etcd/ca.crt \\
-|tee \\
-/var/log/etcd.log
+#|tee \\
+#/var/log/etcd.log
 
 EOF
 
@@ -35,9 +35,9 @@ id=`echo $ip|cut -d '.' -f4`;i=${id: -1}
   docker run \
 --privileged -itd --name etcd-`if [ "$i" == "1" ]; then echo m; else echo s$i; fi` -h $node --net etcd --ip $ip \
     -v ~/.k8s-etcds/cert:/etc/kubernetes/pki/etcd -v ~/.k8s-etcds/$node:/var/lib/etcd -v /etc/hosts:/etc/hosts -v ~/.k8s-etcds/etcd-logs/etcd.log:/var/log/etcd.log -v ~/.k8s-etcds/.start.sh:/root/.start.sh \
-    -e TZ=Asia/Shanghai -e PS1='\w \$ ' -w /root \
+    -e $ip -e TZ=Asia/Shanghai -e PS1='\w \$ ' -w /root \
   swr.cn-east-3.myhuaweicloud.com/vipexcc/k8s/k8s.gcr.io/etcd:3.4.13-0 \
-~/.start.sh
+/root/.start.sh
 done
 
 docker run --rm -it --name cli --net etcd --ip 10.0.1.106 \
